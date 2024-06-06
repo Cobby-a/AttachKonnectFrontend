e08b94a42f28e198b0bad5501d8c65a5861568a5
cobby-a <aduboateng333@gmail.com>
2024-06-06 22:46:47 +0000

    admin company page

--- src/admin/company.js
+++ src/admin/company.js
@@ -1,13 +1,24 @@
 import profile from './assets/profile.png'
-import { faBuilding, faHouse, faBriefcase, faUserTie, faUserGear, faRightFromBracket, faBars, faXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBuilding, faHouse, faBriefcase, faUserTie, faUserGear, faRightFromBracket, faBars, faXmark, faMagnifyingGlass, faPen, faClipboard } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import './company.css'
 import './sidebar.css'
-
+import { data } from './companyData'
 
 const AdminCompany = () => {
     const [menu, setMenu] = useState(false);
+
+    const numbers = [
+        {id : 1},
+        {id : 2},
+        {id : 3},
+        {id : 4},
+        {id : 5},
+        {id : 6},
+        {id : 7},
+    ]
+    // const[data, setData] = useState(companyData);
     return(
         <main className="adminCompanyBody">
             <header>
@@ -48,13 +59,73 @@ const AdminCompany = () => {
                 <article className="mainCompanyBody">
                     <div className='companySearch'>
                         <div>
-                            <h3 style={{color: "#4C4C4C", fontSize: '1.1rem', marginBottom: '0.4rem', fontFamily: 'Montserrat'}}>Companies</h3>
+                            <p style={{ fontSize: '1.2rem', marginBottom: '0.4rem', fontFamily: 'Poppins'}}>Companies</p>
                             <p style={{color: '#B3B3B3', fontSize: '0.8rem'}}>Monitor interns, their contracts and reports.</p>
                         </div>
                         <form>
                             <span><FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize: '1.2rem', padding: "10px 10px 10px 14px", color: '#4C4C4C' }}/></span><input type='search' name='searchCompany' placeholder='Search Company'/>
                         </form>
                     </div>
+                    <div className='table'>
+                    <table className="companydeets">
+                    <thead>
+                    <tr>
+                        <th style={{paddingLeft: '1rem'}}>Logo</th>
+                        <th>Contract status</th>
+                        <th>Report Status</th>
+                        <th>Vacancy</th>
+                        <th>Company</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.map((data)=>{
+                        const {id, logo, contactStatus, reportStatus, vacancy, company} = data;
+                        let classname = "verified";
+
+                        if(contactStatus === 'Pending'){
+                            classname = "pending"
+                        }
+
+                        if(reportStatus === 'Pending'){
+                            classname = "pending"
+                        }
+
+                        return(
+                            <tr key={id}>
+                                <td style={{paddingLeft: '1rem'}}><img src={logo} alt={company} style={{width: "25px", height: "25px", objectFit: 'cover',}}/></td>
+                                <td><span className={classname}>{contactStatus}</span></td>
+                                <td><span className={classname}>{reportStatus}</span></td>
+                                <td>{vacancy}</td>
+                                <td>{company}</td>
+                                <td>
+                                    <span style={{ cursor: 'pointer'}}>
+                                        <FontAwesomeIcon icon={faPen} style={{fontSize: '1.2rem', color: "#1A7AE0"}}/>
+                                    </span>
+                                </td>
+                                <td>    
+                                    <span style={{ cursor: 'pointer'}}>
+                                        <FontAwesomeIcon icon={faClipboard} style={{fontSize: '1.2rem', color: "#1A7AE0"}}/>
+                                    </span>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                    <tr >
+                        <td colSpan='7' style={{backgroundColor: "#F2F2F2", fontSize: '12px', padding: '18px'}}>
+                            <div style={{color: "#9F9F9F", paddingRight: '1rem', display: 'inline-block', cursor: 'pointer'}}>Previous Page</div>
+                            {numbers.map((num)=>{
+                                return(
+                                    <div key={num.id} style={{backgroundColor:"#E6E6E6", color: "#4C4C4C",width: "20px", height: "20px", borderRadius: "50%", textAlign: "center", alignContent: 'center', cursor: 'pointer', marginRight: "0.8rem",display: 'inline-block'}}><span style={{margin: '0'}}>{num.id}</span></div>
+                                )
+                            })}
+                            <div style={{color: "#4C4C4C", display: 'inline-block', cursor: 'pointer'}}>Next Page</div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
                 </article>
             </section>
             <section className='MainContainer'>
@@ -68,8 +139,70 @@ const AdminCompany = () => {
                             <span><FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize: '1.2rem', padding: "10px 10px 10px 14px", color: '#4C4C4C' }}/></span><input type='search' name='searchCompany' placeholder='Search Company'/>
                         </form>
                     </div>
+                    
                 </article>
+                <div className='table'>
+                    <table className="companydeets">
+                    <thead>
+                    <tr>
+                        <th style={{paddingLeft: '1rem',}}>Logo</th>
+                        <th>Contract status</th>
+                        <th>Report Status</th>
+                        <th>Vacancy</th>
+                        <th>Company</th>
+                        <th></th>
+                        <th style={{paddingRight: "4rem"}}></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.map((data)=>{
+                        const {id, logo, contactStatus, reportStatus, vacancy, company} = data;
+                        let classname = "verified";
+
+                        if(contactStatus === 'Pending'){
+                            classname = "pending"
+                        }
+
+                        if(reportStatus === 'Pending'){
+                            classname = "pending"
+                        }
+
+                        return(
+                            <tr key={id}>
+                                <td style={{paddingLeft: '1rem'}}><img src={logo} alt={company} style={{width: "25px", height: "25px", objectFit: 'cover',}}/></td>
+                                <td><span className={classname}>{contactStatus}</span></td>
+                                <td><span className={classname}>{reportStatus}</span></td>
+                                <td>{vacancy}</td>
+                                <td>{company}</td>
+                                <td>
+                                    <span style={{marginRight: '1rem', cursor: 'pointer'}}>
+                                        <FontAwesomeIcon icon={faPen} style={{fontSize: '1.2rem', color: "#1A7AE0"}}/>
+                                    </span>
+                                </td>     
+                                <td>   
+                                    <span style={{marginLeft: '1rem', marginRight: '-1rem', cursor: 'pointer'}}>
+                                        <FontAwesomeIcon icon={faClipboard} style={{fontSize: '1.2rem', color: "#1A7AE0"}}/>
+                                    </span>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                    <tr >
+                        <td colSpan='7' style={{backgroundColor: "#F2F2F2", fontSize: '12px', padding: '18px'}}>
+                            <div style={{color: "#9F9F9F", paddingRight: '1rem', display: 'inline-block', cursor: 'pointer'}}>Previous Page</div>
+                            {numbers.map((num)=>{
+                                return(
+                                    <div key={num.id} style={{backgroundColor:"#E6E6E6", color: "#4C4C4C",width: "20px", height: "20px", borderRadius: "50%", textAlign: "center", alignContent: 'center', cursor: 'pointer', marginRight: "0.8rem",display: 'inline-block'}}><span style={{margin: '0'}}>{num.id}</span></div>
+                                )
+                            })}
+                            <div style={{color: "#4C4C4C", display: 'inline-block', cursor: 'pointer'}}>Next Page</div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
             </section>
+
         </main>
     )
 }
---
+++ src/admin/companyData.js
@@ -0,0 +1,57 @@
+import facebook from "../admin/assets/facebook.svg"
+import google from "../admin/assets/google.svg"
+import apple from "../admin/assets/apple.svg"
+import spotify from "../admin/assets/spotify.svg"
+import instagram from "../admin/assets/instagram.svg"
+import tesla from "../admin/assets/tesla.svg"
+
+export const data = [
+    {
+        id: 1,
+        logo: facebook,
+        contactStatus: "Verified",
+        reportStatus: "Verified",
+        vacancy: "Software Engineer",
+        company: "Company A",
+    },
+    {
+        id: 2,
+        logo: google,
+        contactStatus: "Pending",
+        reportStatus: "Pending",
+        vacancy: "QA Tester",
+        company: "Company B",
+    },
+    {
+        id: 3,
+        logo: apple,
+        contactStatus: "Verified",
+        reportStatus: "Verified",
+        vacancy: "Software Engineer",
+        company: "Company C",
+    },
+    {
+        id: 4,
+        logo: spotify,
+        contactStatus: "Pending",
+        reportStatus: "Pending",
+        vacancy: "QA Tester",
+        company: "Company D",
+    },
+    {
+        id: 5,
+        logo: instagram,
+        contactStatus: "Verified",
+        reportStatus: "Verified",
+        vacancy: "Software Engineer",
+        company: "Company E",
+    },
+    {
+        id: 6,
+        logo: tesla,
+        contactStatus: "Pending",
+        reportStatus: "Pending",
+        vacancy: "QA Tester",
+        company: "Company D",
+    },
+]
