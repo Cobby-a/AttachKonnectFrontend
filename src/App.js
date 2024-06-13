b73745a68ebd6906061d9f74753376ed9c914e44
cobby-a <aduboateng333@gmail.com>
2024-06-13 05:10:40 +0000

    exact route

--- src/App.js
+++ src/App.js
@@ -26,26 +26,26 @@ function App() {
     // <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/'>
-            <Route index element = {<Navigate to= "portal" replace/>}/>
-            <Route path = "/portal" element = {<Auth/>}/>
-            <Route path = "/admin/dashboard" element = {<AdminDashboard/>}/>
-            <Route path = "/admin/companyboard" element = {<AdminCompany/>}/>
-            <Route path = "/admin/vacancyboard" element = {<AdminVacancy/>}/>
-            <Route path = "/admin/applicantsboard" element = {<AdminApplicants/>}/>
-            <Route path = "/admin/manage-users" element = {<AdminManageUsers/>}/>
-            <Route path = "/manager/dashboard" element = {<ManagerDashboard/>}/>
-            <Route path = "/manager/companyboard" element = {<ManagerCompany/>}/>
-            <Route path = "/manager/applicantsboard" element = {<ManagerApplicants/>}/>
-            <Route path = "/supervisor/dashboard" element = {<SupervisorDashboard/>}/>
-            <Route path = "/supervisor/dashboard/register-intern" element = {<SupervisorRegisterIntern/>}/>
-            <Route path = "/supervisor/companyboard" element = {<SupervisorCompany/>}/>
-            <Route path = "/supervisor/manage-users" element = {<SupervisorManageUsers/>}/>
-            <Route path = "/student/dashboard" element = {<StudentDashboard/>}/>
-            <Route path = "/student/companyboard" element = {<StudentCompany/>}/>
-            <Route path = "/student/vacancyboard" element = {<StudentVacancy/>}/>
-            <Route path = "/student/vacancyboard/apply" element = {<StudentApply/>}/>
-            <Route path="*" element={<Navigate to="portal" replace />} />
+          <Route exact path='/'>
+            <Route exact index element = {<Navigate to= "portal" replace/>}/>
+            <Route exact path = "/portal" element = {<Auth/>}/>
+            <Route exact path = "/admin/dashboard" element = {<AdminDashboard/>}/>
+            <Route exact path = "/admin/companyboard" element = {<AdminCompany/>}/>
+            <Route exact path = "/admin/vacancyboard" element = {<AdminVacancy/>}/>
+            <Route exact path = "/admin/applicantsboard" element = {<AdminApplicants/>}/>
+            <Route exact path = "/admin/manage-users" element = {<AdminManageUsers/>}/>
+            <Route exact path = "/manager/dashboard" element = {<ManagerDashboard/>}/>
+            <Route exact path = "/manager/companyboard" element = {<ManagerCompany/>}/>
+            <Route exact path = "/manager/applicantsboard" element = {<ManagerApplicants/>}/>
+            <Route exact path = "/supervisor/dashboard" element = {<SupervisorDashboard/>}/>
+            <Route exact path = "/supervisor/dashboard/register-intern" element = {<SupervisorRegisterIntern/>}/>
+            <Route exact path = "/supervisor/companyboard" element = {<SupervisorCompany/>}/>
+            <Route exact path = "/supervisor/manage-users" element = {<SupervisorManageUsers/>}/>
+            <Route exact path = "/student/dashboard" element = {<StudentDashboard/>}/>
+            <Route exact path = "/student/companyboard" element = {<StudentCompany/>}/>
+            <Route exact path = "/student/vacancyboard" element = {<StudentVacancy/>}/>
+            <Route exact path = "/student/vacancyboard/apply" element = {<StudentApply/>}/>
+            <Route exact path="*" element={<Navigate to="portal" replace />} />
           </Route>
         </Routes>
       </BrowserRouter>
